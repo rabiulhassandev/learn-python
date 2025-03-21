b6d1b7dea3bf4bda81c87af2554954e2e0990b0c
RABIUL HASSAN <rabiulhassandev@gmail.com>
2025-03-22 06:09:54 +0800

    DataType Practice

---
+++ 02.08_DataType_Practice.py
@@ -0,0 +1,23 @@
+number = 10
+print('Number: ', number)
+
+string = "Hello"
+print(f'String: {string}')
+
+list = [1, 4, 5, "hey", 4]
+print('List: ', list)
+
+tuple = (1, 5, 3, 4)
+print("Tuple: ", tuple)
+
+dictionary = {
+    "name" : "Rabiul Hassan",
+    'id' : "65578778876"
+}
+print("Dictionary: ", dictionary)
+
+set = {"apple", 5, "hey"}
+print("Set: ", set)
+
+boolean = False
+print("Boolean: ", boolean)
